c8135330310df91e7f6d539fae954dcce29b030b
Mumer666 <81896836+Mumer666@users.noreply.github.com>
2021-06-19 11:17:29 -0700

    duko farm methods

--- src/dukofarms/DukoFarms.java
+++ src/dukofarms/DukoFarms.java
@@ -38,5 +38,87 @@ private static DukoFarms instance;
         
         return instance;
     }
-    
+     public void addContact(Customer cus){
+        listcustomer.add(cus);
+    }
+     private int searchCustomer(String contact , String name)
+    {
+        int index = -1;
+         for(int i = 0 ; i  < listcustomer.size(); i++)
+         {
+             if(listcustomer.get(i).getContact().equals(contact) ||  listcustomer.get(i).getName().equals(name))
+             {
+                 index = i;
+                 break;
+             }
+         }
+         
+        return index;
+    }
+        private int searchCustomer( String name)
+    {
+        int index = -1;
+         for(int i = 0 ; i  < listcustomer.size(); i++)
+         {
+             if(listcustomer.get(i).getName().equals(name))
+             {
+                 index = i;
+                 break;
+             }
+         }
+         
+        return index;
+    }
+     public Customer getContact(String cnic)
+    {
+        int index = searchCustomer(cnic,"");
+        return listcustomer.get(index);
+    }
+          public boolean updateCustomer(String CNIC ,Customer custo){
+       int index = searchCustomer(CNIC);
+       
+       if(index == -1)
+           return false;
+       else {
+           listcustomer.set(index, custo);
+           return true;
+       }
+    }
+            public boolean deleteCustomer(String CNIC){
+       int index = searchCustomer(CNIC);
+       
+       if(index == -1)
+           return false;
+       else {
+           listcustomer.remove(index);
+           return true;
+       }
+    }
+             public List<Customer> getAllCustomers(){
+        return listcustomer;
+    }
+               public List<Customer> searchBasedOnName(String nameText){
+        List<Customer> searched = new ArrayList<Customer>();
+        
+         for(int i = 0 ; i  < listcustomer.size(); i++)
+         {
+             if(listcustomer.get(i).getName().contains(nameText))
+             {
+                 searched.add(listcustomer.get(i));
+             }
+         }
+        return searched;
+    }
+        public List<Customer> searchBasedOnCNIC(String CNIC){
+        List<Customer> searched = new ArrayList<Customer>();
+        
+         for(int i = 0 ; i  < listcustomer.size(); i++)
+         {
+             if(listcustomer.get(i).getCNIC().contains(CNIC))
+             {
+                 searched.add(listcustomer.get(i));
+             }
+         }
+        return searched;
+    }
 }
